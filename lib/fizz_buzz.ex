608b82c5570816e18826eac324399729ed47541c
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-28 16:58:25 -0300

    initial commit
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ lib/fizz_buzz.ex
@@ -0,0 +1,26 @@
+defmodule FizzBuzz do
+  def build(file_name) do
+    result = File.read(file_name)
+    handle(result)
+  end
+
+  defp handle({:ok, file}), do: handle(file)
+  defp handle({:error, reason}), do: {:error, reason}
+
+  defp handle(file) do
+    file
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce([], fn x, acc -> [fizz_buzz(x) | acc] end)
+  end
+
+  defp fizz_buzz(number) do
+    case {rem(number, 3), rem(number, 5)} do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      _ -> number
+    end
+  end
+end
